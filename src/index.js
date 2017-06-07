786f181bfea81b1bba1d4fef9c938e6c6c675c97
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-06-06 23:31:13 -0700

    Added Picatic favicon and removed manifest from React config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 // FIXME: Need to resolve tap event plugin for Material UI
@@ -9,4 +8,3 @@ import './index.css';
 // injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
